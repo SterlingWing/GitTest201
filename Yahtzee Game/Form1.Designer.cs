f7f996e35dc143f2c38d22eca7d9a3291d4878b4
SterlingWing <sterling.wing@connect.qut.edu.au>
2016-05-09 16:06:15 +1000

    test

---
+++ Yahtzee Game/Form1.Designer.cs
@@ -0,0 +1,224 @@
+﻿namespace Yahtzee_Game {
+    partial class Form1 {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent() {
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.labelDie5 = new System.Windows.Forms.Label();
+            this.labelDie4 = new System.Windows.Forms.Label();
+            this.labelDie3 = new System.Windows.Forms.Label();
+            this.labelDie2 = new System.Windows.Forms.Label();
+            this.labelDie1 = new System.Windows.Forms.Label();
+            this.checkBoxDie1 = new System.Windows.Forms.CheckBox();
+            this.checkBoxDie2 = new System.Windows.Forms.CheckBox();
+            this.checkBoxDie3 = new System.Windows.Forms.CheckBox();
+            this.checkBoxDie4 = new System.Windows.Forms.CheckBox();
+            this.checkBoxDie5 = new System.Windows.Forms.CheckBox();
+            this.buttonRollDice = new System.Windows.Forms.Button();
+            this.labelHoldValueMessage = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer1.Name = "splitContainer1";
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.label1);
+            this.splitContainer1.Panel1.Controls.Add(this.labelHoldValueMessage);
+            this.splitContainer1.Panel1.Controls.Add(this.buttonRollDice);
+            this.splitContainer1.Panel1.Controls.Add(this.checkBoxDie5);
+            this.splitContainer1.Panel1.Controls.Add(this.checkBoxDie4);
+            this.splitContainer1.Panel1.Controls.Add(this.checkBoxDie3);
+            this.splitContainer1.Panel1.Controls.Add(this.checkBoxDie2);
+            this.splitContainer1.Panel1.Controls.Add(this.checkBoxDie1);
+            this.splitContainer1.Panel1.Controls.Add(this.labelDie5);
+            this.splitContainer1.Panel1.Controls.Add(this.labelDie4);
+            this.splitContainer1.Panel1.Controls.Add(this.labelDie3);
+            this.splitContainer1.Panel1.Controls.Add(this.labelDie2);
+            this.splitContainer1.Panel1.Controls.Add(this.labelDie1);
+            this.splitContainer1.Size = new System.Drawing.Size(711, 607);
+            this.splitContainer1.SplitterDistance = 474;
+            this.splitContainer1.TabIndex = 0;
+            // 
+            // labelDie5
+            // 
+            this.labelDie5.BackColor = System.Drawing.Color.White;
+            this.labelDie5.Location = new System.Drawing.Point(280, 27);
+            this.labelDie5.Name = "labelDie5";
+            this.labelDie5.Size = new System.Drawing.Size(40, 40);
+            this.labelDie5.TabIndex = 4;
+            this.labelDie5.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // labelDie4
+            // 
+            this.labelDie4.BackColor = System.Drawing.Color.White;
+            this.labelDie4.Location = new System.Drawing.Point(220, 27);
+            this.labelDie4.Name = "labelDie4";
+            this.labelDie4.Size = new System.Drawing.Size(40, 40);
+            this.labelDie4.TabIndex = 3;
+            this.labelDie4.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // labelDie3
+            // 
+            this.labelDie3.BackColor = System.Drawing.Color.White;
+            this.labelDie3.Location = new System.Drawing.Point(160, 27);
+            this.labelDie3.Name = "labelDie3";
+            this.labelDie3.Size = new System.Drawing.Size(40, 40);
+            this.labelDie3.TabIndex = 2;
+            this.labelDie3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // labelDie2
+            // 
+            this.labelDie2.BackColor = System.Drawing.Color.White;
+            this.labelDie2.Location = new System.Drawing.Point(100, 27);
+            this.labelDie2.Name = "labelDie2";
+            this.labelDie2.Size = new System.Drawing.Size(40, 40);
+            this.labelDie2.TabIndex = 1;
+            this.labelDie2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // labelDie1
+            // 
+            this.labelDie1.BackColor = System.Drawing.Color.White;
+            this.labelDie1.Location = new System.Drawing.Point(40, 27);
+            this.labelDie1.Name = "labelDie1";
+            this.labelDie1.Size = new System.Drawing.Size(40, 40);
+            this.labelDie1.TabIndex = 0;
+            this.labelDie1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // checkBoxDie1
+            // 
+            this.checkBoxDie1.AutoSize = true;
+            this.checkBoxDie1.Location = new System.Drawing.Point(52, 80);
+            this.checkBoxDie1.Name = "checkBoxDie1";
+            this.checkBoxDie1.Size = new System.Drawing.Size(15, 14);
+            this.checkBoxDie1.TabIndex = 5;
+            this.checkBoxDie1.UseVisualStyleBackColor = true;
+            // 
+            // checkBoxDie2
+            // 
+            this.checkBoxDie2.AutoSize = true;
+            this.checkBoxDie2.Location = new System.Drawing.Point(112, 80);
+            this.checkBoxDie2.Name = "checkBoxDie2";
+            this.checkBoxDie2.Size = new System.Drawing.Size(15, 14);
+            this.checkBoxDie2.TabIndex = 6;
+            this.checkBoxDie2.UseVisualStyleBackColor = true;
+            // 
+            // checkBoxDie3
+            // 
+            this.checkBoxDie3.AutoSize = true;
+            this.checkBoxDie3.Location = new System.Drawing.Point(172, 80);
+            this.checkBoxDie3.Name = "checkBoxDie3";
+            this.checkBoxDie3.Size = new System.Drawing.Size(15, 14);
+            this.checkBoxDie3.TabIndex = 7;
+            this.checkBoxDie3.UseVisualStyleBackColor = true;
+            // 
+            // checkBoxDie4
+            // 
+            this.checkBoxDie4.AutoSize = true;
+            this.checkBoxDie4.Location = new System.Drawing.Point(232, 80);
+            this.checkBoxDie4.Name = "checkBoxDie4";
+            this.checkBoxDie4.Size = new System.Drawing.Size(15, 14);
+            this.checkBoxDie4.TabIndex = 8;
+            this.checkBoxDie4.UseVisualStyleBackColor = true;
+            // 
+            // checkBoxDie5
+            // 
+            this.checkBoxDie5.AutoSize = true;
+            this.checkBoxDie5.Location = new System.Drawing.Point(292, 80);
+            this.checkBoxDie5.Name = "checkBoxDie5";
+            this.checkBoxDie5.Size = new System.Drawing.Size(15, 14);
+            this.checkBoxDie5.TabIndex = 9;
+            this.checkBoxDie5.UseVisualStyleBackColor = true;
+            // 
+            // buttonRollDice
+            // 
+            this.buttonRollDice.Location = new System.Drawing.Point(340, 36);
+            this.buttonRollDice.Name = "buttonRollDice";
+            this.buttonRollDice.Size = new System.Drawing.Size(90, 23);
+            this.buttonRollDice.TabIndex = 10;
+            this.buttonRollDice.Text = "Click to roll dice";
+            this.buttonRollDice.UseVisualStyleBackColor = true;
+            // 
+            // labelHoldValueMessage
+            // 
+            this.labelHoldValueMessage.BackColor = System.Drawing.Color.White;
+            this.labelHoldValueMessage.Location = new System.Drawing.Point(337, 71);
+            this.labelHoldValueMessage.Name = "labelHoldValueMessage";
+            this.labelHoldValueMessage.Size = new System.Drawing.Size(76, 30);
+            this.labelHoldValueMessage.TabIndex = 11;
+            this.labelHoldValueMessage.Text = "Check box to hold value";
+            this.labelHoldValueMessage.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(44, 132);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(64, 25);
+            this.label1.TabIndex = 12;
+            this.label1.Text = "label1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.DodgerBlue;
+            this.ClientSize = new System.Drawing.Size(711, 607);
+            this.Controls.Add(this.splitContainer1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.Label labelDie3;
+        private System.Windows.Forms.Label labelDie2;
+        private System.Windows.Forms.Label labelDie1;
+        private System.Windows.Forms.Label labelDie5;
+        private System.Windows.Forms.Label labelDie4;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label labelHoldValueMessage;
+        private System.Windows.Forms.CheckBox checkBoxDie5;
+        private System.Windows.Forms.CheckBox checkBoxDie4;
+        private System.Windows.Forms.CheckBox checkBoxDie3;
+        private System.Windows.Forms.CheckBox checkBoxDie2;
+        private System.Windows.Forms.CheckBox checkBoxDie1;
+        private System.Windows.Forms.Button buttonRollDice;
+    }
+}
+
