3356261333b6e364787f6a45b91accd17ded5802
TylerMcK <tiga@hotmail.com.au>
2016-05-13 02:09:15 +1000

    Finished for night

--- Yahtzee Game/Form1.cs
+++ Yahtzee Game/Form1.cs
@@ -17,6 +17,7 @@ namespace Yahtzee_Game {
 
         public Form1() {
             InitializeComponent();
+            InitialiseLabelsAndButtons();
         }
 
         private void InitialiseLabelsAndButtons() {
