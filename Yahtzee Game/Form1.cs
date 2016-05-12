49fff1c4dc77020261d48b5746c7cb7d53556ecd
TylerMcK <tiga@hotmail.com.au>
2016-05-12 13:39:50 +1000

    Added methods and variables

--- Yahtzee Game/Form1.cs
+++ Yahtzee Game/Form1.cs
@@ -10,9 +10,74 @@ using System.Windows.Forms;
 
 namespace Yahtzee_Game {
     public partial class Form1 : Form {
+        private Array[] dice;
+        private Array[] scoreButtons;
+        private Array[] scoreTotals;
+        private Array[] checkBoxes;
+        private string game;
 
         public Form1() {
             InitializeComponent();
+
+        }
+
+        private void InitialiseLabelsAndButtons() {
+
+        }
+
+        public void GetDice() {
+
+        }
+
+        public void GetScoresTotals() {
+
+        }
+
+        public void ShowPlayerName(string name) {
+
+        }
+
+        public void EnableRollButton() {
+
+        }
+
+        public void DisableRollButton() {
+
+        }
+
+        public void EnableCheckBoxes() {
+
+        }
+
+        public void DisableAndClearCheckBoxes() {
+
+        }
+
+        public void EnableScoreButton(ScoreType combo) {
+
+        }
+
+        public void DisableScoreButton(ScoreType combo) {
+
+        }
+
+        public void CheckCheckBox(int index) {
+
+        }
+
+        public void ShowMessage(string message) {
+
+        }
+
+        public void ShowOKButton() {
+
+        }
+
+        public void StartNewGame() {
+
+        }
+
+        private void buttonRollDice_Click(object sender, EventArgs e) {
 
         }
     }
